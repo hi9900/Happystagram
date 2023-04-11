504001f50d5d9929861834cfc942e35105ddc9f9
HY <9hiyoung@gmail.com>
2023-04-11 21:31:24 +0900

    ADD: index
    230411

---
+++ posts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from .forms import PostForm
+
+# Create your views here.
+def index(request):
+    return render(request, 'posts/index.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm()        
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/create.html', context)
+
